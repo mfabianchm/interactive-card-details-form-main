937cb0d3c58f5e6e56ed9478ef6bb74b56a03210
mfabianchm <mfchongm@hotmail.com>
2024-03-02 23:01:02 -0600

    Initial Commit

---
+++ interactive-card-details-form-main/interactive_card_details_form_mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_card_details_form_mainApp.swift
+//  interactive-card-details-form-main
+//
+//  Created by Marcos Fabian Chong Megchun on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct interactive_card_details_form_mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
